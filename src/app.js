07fb43b4671af6a441cd4128041e3d1f739e450c
Andressa Souza <afariasouza@outlook.com>
2025-03-25 16:13:42 -0300

    Conexão com MongoDB usando Mongoose

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import conectaNaDatabase from './config/dbConnect.js';
+
+const conexao = await conectaNaDatabase();
+
+conexao.on('error', (erro) => {
+	console.error('erro de conexão', erro);
+});
+
+conexao.once('open', () => {
+	console.log('Conexão com o banco realizada com sucesso!');
+});
 
 const app = express();
 app.use(express.json());
